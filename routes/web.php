130d3ce3c730f962f574c510b4fa262cfc477cf0
= <=>
2023-05-23 19:34:02 +0700

    Praktikum 3: Membuat Fitur Export PDF

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::resource('articles', ArticleController::class);
 
 
 // Auth::routes();
-
+Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf']);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
